d2876ef8da35007f5031c4d7bf6eb920eca30e1f
Ben Crook <ben@space48.com>
2018-02-04 16:55:42 +0000

    Join CSS objects together so both can be used with styles.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-import styles from './App.css';
+import appStyles from './App.css';
 import buttonStyles from './ui/button/Button.css';
 
 import Button from './ui/button/Button';
+
+// Combine styles
+let styles = {};
+Object.assign(styles, appStyles, buttonStyles)
+
 
 class App extends Component {
   render() {
@@ -13,8 +18,8 @@ class App extends Component {
         <header className={styles.header}>
           <img src={logo} className={styles.logo} alt="logo" />
           <h1 className={styles.title}>Welcome to React</h1>
-            <Button modifier={`${buttonStyles.red} ${buttonStyles.large}`} link={"//google.com"} text={"Test Button"} />
-            <Button modifier={buttonStyles.red} link={"//google.com"} text={"Test Button"} />
+            <Button modifier={`${styles.red} ${styles.large}`} link={"//google.com"} text={"Test Button"} />
+            <Button modifier={styles.red} link={"//google.com"} text={"Test Button"} />
         </header>
         <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
