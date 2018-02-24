54bc688e5b43c628cfb986b766950f7efae4caec
Ben Crook <ben@space48.com>
2018-02-24 21:05:30 +0000

    Refactor product listing so products.js can be removed

--- src/components/productListing/product-listing.js
+++ src/components/productListing/product-listing.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+
+import Button from "../button/button";
 import Filter from './filter/filter.js';
-import Product from './products/products.js';
 
 import styles from './product-listing.css';
+import stylesProduct from './products.css';
 
 class ProductListing extends Component {
     constructor(props) {
@@ -16,21 +18,50 @@ class ProductListing extends Component {
         };
 
         // Bind functions to ProductListing
+        this.addToBag = this.addToBag.bind(this);
+        this.productHtml = this.productHtml.bind(this);
         this.getProducts = this.getProducts.bind(this);
         this.checkHandler = this.checkHandler.bind(this);
         this.textHandler = this.textHandler.bind(this);
     }
 
+    addToBag(product) {
+        this.props.addToBagEvent({price: product.price, stocked: product.stocked, name: product.name});
+    }
+
+    productHtml(index, product) {
+        return (
+            <div key={index} className={stylesProduct.product}>
+                <div>
+                    <img className={stylesProduct.productImage} src="http://via.placeholder.com/600x600" alt=""/>
+                </div>
+                <div className={stylesProduct.productName}>{product.name}</div>
+                <div className={stylesProduct.productPrice}>{product.price}</div>
+                <div className={stylesProduct.productStock}>{product.stocked}</div>
+                <Button link={() => this.addToBag(product)} text="Add To Cart"/>
+            </div>
+        )
+    }
+
     getProducts() {
         let products = [];
-        this.props.products.forEach((product) => {
+
+
+        this.props.products.map((product, index) => {
             if (this.state.isFilteredByStock && !product.stocked) {
-                return;
+                return false;
             }
+
             if (!product.name.toLowerCase().includes(this.state.filterText.toLowerCase())) {
-                return;
+                return false;
             }
-            products.push(<Product addToBagEvent={this.props.addToBagEvent} key={product.sku} name={product.name} price={product.price} inStock={product.stocked} />);
+
+            products.push(this.productHtml(index, product));
+
+            return (<div key={index} data-test={index}>
+                <span>{product.name}</span>
+                <span>£{product.price}</span>
+            </div>)
         });
 
         return products;
